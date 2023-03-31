122e1900eef219bb7fbd6c4a653df8533f5c07bd
Wruschika <wruschikabakane@gmail.com>
2023-03-31 23:44:58 +0200

    second commit

--- src/test/java/stepdefinations/testcases.java
+++ src/test/java/stepdefinations/testcases.java
@@ -62,29 +62,6 @@ request = RestAssured.given().log().all().headers("Content-Type","application/js
 	    System.out.println(id);
 	}
 
-	@Given("delete user API and id")
-	public void delete_user_api_and_id() {
-		
-	    // Write code here that turns the phrase above into concrete actions
-		
-		RestAssured.baseURI= BASE_URL;
-		//String token ="f22715ae85220457779b9a82f3a3fb4e833f789a92d814146ce9421807a8911f";
-		//String authToken ="Bearer "+token;
-
-		request = RestAssured.given().log().all().headers("Content-Type","application/json","Accept","application/json","Authorization",authToken);
-	}
-	@When("user calls {string} with delete request")
-	public void user_calls_with_delete_request(String string) {
-	    // Write code here that turns the phrase above into concrete actions
-		response = request.when().delete("public/v2/users/+id+");
-	}
-	@Then("API call success with status code is {int}")
-	public void api_call_success_with_status_code_is(Integer int1) {
-	    // Write code here that turns the phrase above into concrete actions
-		response.then().log().all().extract().statusCode();
-	    Assert.assertEquals(response.getStatusCode(),404);
-	}
-	
 	
 	@Given("Get user API")
 	public void get_user_api() {
@@ -110,21 +87,58 @@ request = RestAssured.given().log().all().headers("Content-Type","application/js
 	
 	}
 	@Given("update user API and id")
-	public void update_user_api_and_id() {
-	    // Write code here that turns the phrase above into concrete actions
-	    throw new io.cucumber.java.PendingException();
+	public void update_user_api_and_id()throws IOException {
+	   
+		    // Write code here that turns the phrase above into concrete actions
+			dataDriven d=new dataDriven();
+			ArrayList<String> data=d.getData("Update");
+			
+			
+			HashMap<String, Object>  jsonAsMap = new HashMap<>();
+			jsonAsMap.put("name", data.get(1));
+			jsonAsMap.put("gender", data.get(2));
+			jsonAsMap.put("email", data.get(3));
+			jsonAsMap.put("status", data.get(4));
+			
+	RestAssured.baseURI= BASE_URL;
+
+	request = RestAssured.given().log().all().headers("Content-Type","application/json","Accept","application/json","Authorization",authToken)
+	.body(jsonAsMap);
 	}
 	@When("user calls {string} with put request")
 	public void user_calls_with_put_request(String string) {
 	    // Write code here that turns the phrase above into concrete actions
-	    throw new io.cucumber.java.PendingException();
+		response = request.when().put("public/v2/users/+id+");
 	}
 	@Then("API call success with status code as {int}")
 	public void api_call_success_with_status_code_as(Integer int1) {
 	    // Write code here that turns the phrase above into concrete actions
-	    throw new io.cucumber.java.PendingException();
+		int actualresponsecode = response.then().log().all().extract().statusCode();
+	    Assert.assertEquals(response.getStatusCode(),200);
+	   
 	}
 
-	
+	@Given("delete user API and id")
+	public void delete_user_api_and_id() {
+		
+	    // Write code here that turns the phrase above into concrete actions
+		
+		RestAssured.baseURI= BASE_URL;
+		//String token ="f22715ae85220457779b9a82f3a3fb4e833f789a92d814146ce9421807a8911f";
+		//String authToken ="Bearer "+token;
+
+		request = RestAssured.given().log().all().headers("Content-Type","application/json","Accept","application/json","Authorization",authToken);
+	}
+	@When("user calls {string} with delete request")
+	public void user_calls_with_delete_request(String string) {
+	    // Write code here that turns the phrase above into concrete actions
+		response = request.when().delete("public/v2/users/+id+");
+	}
+	@Then("API call success with status code is {int}")
+	public void api_call_success_with_status_code_is(Integer int1) {
+	    // Write code here that turns the phrase above into concrete actions
+		response.then().log().all().extract().statusCode();
+	    Assert.assertEquals(response.getStatusCode(),404);
+	}
 }
 
